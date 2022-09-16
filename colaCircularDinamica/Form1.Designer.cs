0c5956626eccae9f949bbe75194fa2d9009a0f95
EstefDmz <94266169+EstefDmz@users.noreply.github.com>
2022-09-16 17:57:53 -0500

    Add files via upload

---
+++ colaCircularDinamica/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace colaCircularDinamica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtCola = new System.Windows.Forms.TextBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(12, 12);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(156, 20);
+            this.txtDato.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(174, 10);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(109, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtCola
+            // 
+            this.txtCola.Location = new System.Drawing.Point(12, 46);
+            this.txtCola.Multiline = true;
+            this.txtCola.Name = "txtCola";
+            this.txtCola.ReadOnly = true;
+            this.txtCola.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCola.Size = new System.Drawing.Size(271, 160);
+            this.txtCola.TabIndex = 2;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(12, 215);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(271, 23);
+            this.btnDel.TabIndex = 3;
+            this.btnDel.Text = "Quitar";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 251);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.txtCola);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtDato);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtCola;
+        private System.Windows.Forms.Button btnDel;
+    }
+}
+
---
+++ colaCircularDinamica/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace colaCircularDinamica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Cola circular = new Cola();
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            circular.Agregar(txtDato.Text);
+            txtDato.Clear();
+            txtCola.Text = circular.ToString();
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            circular.Eliminar();
+            txtCola.Text = circular.ToString();
+        }
+    }
+}
